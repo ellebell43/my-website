d39ca9f00fe9303d36e6e3e34ddf9d136799b28b
Elle Brooks <ellebell43@proton.me>
2025-07-31 21:22:46 -0700

    spacing fixes

--- app/components/project-card.tsx
+++ app/components/project-card.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function ProjectCard(props: { title: string, href: string, description?: string, right?: boolean }) {
   return (
-    <Link href={props.href} className={`bg-purple-100 border-2 rounded hover:bg-purple-200 block transition-all p-4 shadow-lg w-[300px] md:w-[450px] h-[150px] overflow-scroll ${props.right ? "self-end" : ""}`}>
-      <p className={`text-xl font-bold mb-4 ${props.right ? "text-right" : ""}`}>{props.title}</p>
+    <Link href={props.href} className={`bg-purple-100 border-2 rounded hover:bg-purple-200 block transition-all p-4 shadow-lg w-[300px] md:w-[450px] h-[175px] overflow-scroll ${props.right ? "self-end" : ""}`}>
+      <p className={`text-2xl font-bold mb-4 ${props.right ? "text-right" : ""}`}>{props.title}</p>
       {props.description ? <p className={``}>{props.description}</p> : <></>}
     </Link>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -11,14 +11,14 @@ export default function Page() {
   return (
     <div className={`p-6 from-blue-100 to-purple-200 bg-gradient-to-br min-h-screen ${winkySans.className}`}>
       <div className="max-w-[750px] mx-auto relative">
-        <h1 className="my-[120px] md:text-7xl text-6xl text-right">Hello 👋 I'm Elle!</h1>
-        <p className="text-xl my-2">Welcome to my little corner of the internet! I'm just some random transfem girl from PDX with a big interest in technology 🖥️ This is were you can find links to most of my projects and socials. I think I might get into blogging about what I do with my time. I won't bother with any kind of newsletter if I do, though, but I will post about it on Mastodon 🙂 So, be sure to just check there periodically!</p>
+        <h1 className="my-[120px] md:text-7xl text-6xl text-right">Hi 👋 I'm Elle!</h1>
+        <p className="text-xl my-3">Welcome to my little corner of the internet! I'm just some random transfem girl from PDX with a big interest in technology 🖥️ This is were you can find links to most of my projects and socials. I think I might get into blogging about what I do with my time. I won't bother with any kind of newsletter if I do, though, but I will post about it on Mastodon 🙂 So, be sure to just check there periodically!</p>
 
-        <p className="text-xl my-2">I really enjoy self-hosting things, and all my projects (including this site) are self-hosted. I'm currently working on setting up an email server. I have a couple other projects that are usable, but not listed here due to having less secure connections.</p>
+        <p className="text-xl my-4">I really enjoy self-hosting things, and all my projects (including this site) are self-hosted. I'm currently working on setting up an email server. I have a couple other projects that are usable, but not listed here due to having less secure connections.</p>
 
         <h2 className="border-b-2 mt-12 mb-6 text-4xl pb-4">Projects</h2>
         <div className="flex flex-col gap-8">
-          <ProjectCard href="/mapper" title="Traveller RPG Mapper" description="A fun little tool for randomly generating hex maps for the game Traveller (a TTRPG)" />
+          <ProjectCard href="/mapper" title="(WIP) Traveller Mapper" description="A fun little tool for randomly generating hex maps for the game Traveller (a TTRPG)" />
           <ProjectCard href="https://recipes.ellebell.dev/g/the-council/" title="Mealie" description="My self hosted recipe manager. Got some recipes for me to add? DM me!" right={true} />
           <ProjectCard href="https://movies.ellebell.dev/" title="Jellyfin" description="My media browser/player to host and stream all the movies that I own. You won't be able to access it without an account." />
           <p className="text-xl">I'm also running servers locally for Joplin (notes and to-dos), reverse proxy, and dynamic DNS!</p>
