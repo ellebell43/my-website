5226f29fc9e419d40fe93297eab59ae05893f0ca
Elle Brooks <ellebell43@proton.me>
2025-08-01 09:14:38 -0700

    better contrast

--- app/components/project-card.tsx
+++ app/components/project-card.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function ProjectCard(props: { title: string, href: string, description?: string, right?: boolean }) {
   return (
-    <Link href={props.href} className={`bg-purple-100 border-2 rounded hover:bg-purple-200 block transition-all p-4 shadow-lg w-[300px] md:w-[450px] h-[175px] md:h-[150px] overflow-scroll ${props.right ? "self-end" : ""}`}>
+    <Link href={props.href} className={`bg-violet-50 border-2 rounded hover:bg-violet-100 block transition-all p-4 shadow-lg w-[300px] md:w-[450px] h-[175px] md:h-[150px] overflow-scroll ${props.right ? "self-end" : ""}`}>
       <p className={`text-2xl font-bold mb-4 ${props.right ? "text-right" : ""}`}>{props.title}</p>
       {props.description ? <p className={``}>{props.description}</p> : <></>}
     </Link>
--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,21 @@ import Link from "next/link"
 import ProjectCard from "./components/project-card"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faDiscord, faGithub, faMastodon } from "@fortawesome/free-brands-svg-icons"
+import { faComputer, faFaceGrin, faFish, faHippo, faKiwiBird, faSmile } from "@fortawesome/free-solid-svg-icons"
+import Image from "next/image"
 
 const winkySans = Winky_Sans({ weight: "500", subsets: ["latin"] })
 
 
 export default function Page() {
   return (
-    <div className={`p-6 from-blue-100 to-purple-200 bg-gradient-to-br min-h-screen ${winkySans.className}`}>
+    <div className={`p-6 from-blue-50 to-purple-100 bg-gradient-to-br min-h-screen ${winkySans.className}`}>
       <div className="max-w-[750px] mx-auto relative">
-        <h1 className="my-[120px] md:text-7xl text-6xl text-right">Hi 👋 I'm Elle!</h1>
-        <p className="text-xl my-3">Welcome to my little corner of the internet! I'm just some random transfem girl from PDX with a big interest in technology 🖥️ This is were you can find links to most of my projects and socials. I think I might get into blogging about what I do with my time. I won't bother with any kind of newsletter if I do, though, but I will post about it on Mastodon 🙂 So, be sure to just check there periodically!</p>
+        <div className="my-[120px] relative flex md:flex-row flex-col justify-end items-end gap-6">
+
+          <h1 className="md:text-7xl text-6xl text-right flex flex-col md:flex-row gap-2"><span><FontAwesomeIcon width={84} className="self-end inline relative bottom-4 -rotate-60" icon={faKiwiBird} /> Hi,</span><span> I'm Elle!</span></h1>
+        </div>
+        <p className="text-xl my-3">Welcome to my little corner of the internet! I'm just some random transfem girl from PDX with a big interest in technology <FontAwesomeIcon className="inline" width={20} icon={faComputer} /> This is were you can find links to most of my projects and socials. I think I might get into blogging about what I do with my time. I won't bother with any kind of newsletter if I do, though, but I will post about it on Mastodon <FontAwesomeIcon width={18} className="inline relative bottom-1" icon={faSmile} /> So, be sure to just check there periodically!</p>
 
         <p className="text-xl my-4">I really enjoy self-hosting things, and all my projects (including this site) are self-hosted. I'm currently working on setting up an email server. I have a couple other projects that are usable, but not listed here due to having less secure connections.</p>
 
@@ -20,8 +25,7 @@ export default function Page() {
         <div className="flex flex-col gap-8">
           <ProjectCard href="/mapper" title="(WIP) Traveller Mapper" description="A fun little tool for randomly generating hex maps for the game Traveller (a TTRPG)" />
           <ProjectCard href="https://recipes.ellebell.dev/g/the-council/" title="Mealie" description="My self hosted recipe manager. Got some recipes for me to add? DM me!" right={true} />
-          <ProjectCard href="https://movies.ellebell.dev/" title="Jellyfin" description="My media server to host and stream all the movies that I own. You won't be able to access it without an account." />
-          <p className="text-xl">I'm also running servers locally for Joplin (notes and to-dos), reverse proxy, and dynamic DNS!</p>
+          <p className="text-xl">I'm also running servers locally for Jellyfin (streaming my movies), Joplin (notes and to-dos), reverse proxy, and dynamic DNS!</p>
         </div>
 
         <div className="flex justify-center gap-6 mt-12 text-5xl">
