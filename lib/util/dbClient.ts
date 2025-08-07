e5151bd30902f90b9746be2e7b5c3233652d89c5
Elle Brooks <ellebell43@proton.me>
2025-08-07 14:33:07 -0700

    Update dbClient.ts

--- lib/util/dbClient.ts
+++ lib/util/dbClient.ts
@@ -1,7 +1,5 @@
 import { MongoClient } from "mongodb";
 
-// mongodb://travellerDev:d74upScxxNR5N9Zf97CA@db.ellebell.dev:27017/?authsource=traveller
-
 export async function dbClient() {
   return (await new MongoClient(process.env.MAPPER_DB_STRING).connect()).db("traveller");
 }
