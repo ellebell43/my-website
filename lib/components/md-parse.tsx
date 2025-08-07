0d0a2e1ad81da2dbc5b89f7226c2369169e41622
Elle Brooks <ellebell43@proton.me>
2025-08-07 15:14:39 -0700

    Update md-parse.tsx

--- lib/components/md-parse.tsx
+++ lib/components/md-parse.tsx
@@ -4,7 +4,7 @@ import parse from "html-react-parser"
 export default function MDParse(props: { content: string }) {
   const htmlContent = marked.parse(props.content)
   return (
-    <div className='border-l pl-3 bg-slate-50 my-2 max-h-[300px] h-fit overflow-y-scroll'>
+    <div className='border-l pl-3 bg-slate-50 dark:bg-slate-600 my-2 max-h-[300px] h-fit overflow-y-scroll'>
       {typeof htmlContent === "string" ? parse(htmlContent) : <></>}
     </div>
   )
