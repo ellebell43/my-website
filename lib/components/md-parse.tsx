ca3371d5bcaeacc2e0d2514f788837ff1d8212f8
Elle Brooks <ellebell43@proton.me>
2025-08-08 11:37:22 -0700

    x axis padding increase

--- lib/components/md-parse.tsx
+++ lib/components/md-parse.tsx
@@ -4,7 +4,7 @@ import parse from "html-react-parser"
 export default function MDParse(props: { content: string }) {
   const htmlContent = marked.parse(props.content)
   return (
-    <div className='border-l pl-3 bg-slate-50 dark:bg-slate-600 my-2 max-h-[700px] h-fit overflow-y-scroll text-justify indent-4'>
+    <div className='border-l px-3 bg-slate-50 dark:bg-slate-600 my-2 max-h-[700px] h-fit overflow-y-scroll text-justify indent-4'>
       {typeof htmlContent === "string" ? parse(htmlContent) : <></>}
     </div>
   )
