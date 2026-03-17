74eb6af7c6633ac52c9b4f6d7feca1f8007ca916
ellebell43 <hello@ellebell.dev>
2026-03-17 09:13:52 -0700

    main page update - updated bio blurb - added a project card for hollowglade - updated fediverse link - removed blurb under the project cards

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 import ProjectCard from "../lib/components/project-card"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faDiscord, faGithub, faMastodon } from "@fortawesome/free-brands-svg-icons"
-import { faComputer, faKiwiBird, faSmile } from "@fortawesome/free-solid-svg-icons"
+import { faComputer, faKiwiBird, faSmile, faHandLizard } from "@fortawesome/free-solid-svg-icons"
 import { Metadata } from "next"
 import PrimaryPageWrapper from "@/lib/components/primary-page-wrapper"
 
@@ -14,24 +14,22 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <PrimaryPageWrapper>
-      <div className="my-[120px] relative flex md:flex-row flex-col justify-end items-end gap-6">
+      <div className="my-30 relative flex md:flex-row flex-col justify-end items-end gap-6">
 
         <h1 className="md:text-7xl text-6xl text-right flex flex-col md:flex-row gap-2 border-0"><span><FontAwesomeIcon width={84} className="self-end inline relative bottom-4 -rotate-60" icon={faKiwiBird} /> Hi,</span><span> I'm Elle!</span></h1>
       </div>
-      <p className="text-xl my-3">Welcome to my little corner of the internet! I'm just some random transfem girl from PDX with a big interest in technology <FontAwesomeIcon className="inline" width={20} icon={faComputer} /> This is were you can find links to most of my projects and socials. I think I might get into blogging about what I do with my time. I won't bother with any kind of newsletter if I do, though, but I will post about it on Mastodon <FontAwesomeIcon width={18} className="inline relative bottom-0.5" icon={faSmile} /> So, be sure to just check there periodically!</p>
-
-      <p className="text-xl my-4">I really enjoy self-hosting things, and all my projects (including this site) are self-hosted. I'm currently working on setting up an email server. I have a couple other projects that are usable, but not listed here due to having less secure connections.</p>
+      <p className="text-xl my-3">Welcome to my little corner of the internet. I'm just some random transfem girl from PDX. I love programming, self-hosting, pinball, board games, and table top role play games. Here are my projects! Want to say hi? Find me <Link href="https://social.ellebell.dev/@ellebell43">on the Fediverse</Link>!</p>
 
       <h2 className="border-b-2 mt-12 mb-6 text-4xl pb-4">Projects</h2>
-      <div className="flex flex-col gap-8 max-w-[750px] mx-auto">
+      <div className="flex flex-col gap-8 max-w-187.5 mx-auto">
+        <ProjectCard href="/root/hollowglade" title="Hollowglade Woodland" description="The setting for my current Root TTRPG campaign." right={true} />
         <ProjectCard href="/mapper" title="Traveller Mapper" description="A fun little tool for randomly generating, editing, and saving hex maps for the table top role play game, Traveller" />
         <ProjectCard href="/kendoris" title="Kendoris Subsector" description="A map of Kendoris Subsector, the setting for my current Traveller game that I'm running." right={true} />
         <ProjectCard href="https://recipes.ellebell.dev/g/the-council/" title="Mealie" description="My self hosted recipe manager. Got some recipes for me to add? DM me!" />
-        <p className="text-xl">I'm also running servers locally for Jellyfin (streaming my movies), Joplin (notes and to-dos), reverse proxy, and dynamic DNS!</p>
       </div>
 
       <div className="flex justify-center gap-6 mt-12 text-5xl">
-        <Link rel="me" href="https://cathode.church/@ellebell43"><FontAwesomeIcon className="w-12 transition-all hover:opacity-80" icon={faMastodon} /></Link>
+        <Link rel="me" href="https://social.ellebell.dev/@ellebell43"><FontAwesomeIcon className="w-12 transition-all hover:opacity-80" icon={faMastodon} /></Link>
         <Link href="https://github.com/ellebell43/my-website"><FontAwesomeIcon className="w-12 transition-all hover:opacity-80" icon={faGithub} /></Link>
         <Link href="https://discord.com/users/ellebell43"><FontAwesomeIcon className="w-12 transition-all hover:opacity-80" icon={faDiscord} /></Link>
       </div>
