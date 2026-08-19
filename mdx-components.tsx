e97ea44f2f4561cd924e16c2096d3725adc401d9
Elle Brooks <hello@ellebell.dev>
2026-08-19 16:48:25 -0700

    Moved files around to fix mdx support

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,6 +1,8 @@
 import type { MDXComponents } from 'mdx/types'
 
-const components: MDXComponents = {}
+const components: MDXComponents = {
+
+}
 
 export function useMDXComponents(): MDXComponents {
   return components
